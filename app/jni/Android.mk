189ed4e9dc530be8b18a60f15ebeaf42376f8a03
devingryu <devingryu@korea.ac.kr>
2021-05-08 10:13:39 +0900

    Customized Android.mk

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,7 +1,8 @@
 
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-include $(LOCAL_PATH)/rubberband/Android.mk
+include $(LOCAL_PATH)/Android2.mk
 
 $(call import-module,android/cpufeatures)
 
